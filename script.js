3ba6ae8811fb6f34d7cb35ffbc9386b0911236eb
Phu <Phuripattaraphan3@gmail.com>
2025-03-03 18:19:00 +0700

    add date UI

--- script.js
+++ script.js
@@ -10,6 +10,18 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-05-27T17:01:17.194Z",
+    "2020-07-11T23:36:17.929Z",
+    "2020-07-12T10:51:36.790Z",
+  ],
+  currency: "EUR",
+  locale: "pt-PT", // de-DE
 };
 
 const account2 = {
@@ -17,23 +29,21 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
-const account3 = {
-  owner: "Steven Thomas Williams",
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: "Sarah Smith",
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2];
 
 // Elements
 const labelWelcome = document.querySelector(".welcome");
@@ -61,16 +71,27 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
-function displayMovement(movements, sort = false) {
+function displayMovement(acc, sort = false) {
   containerMovements.innerHTML = "";
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  const movs = sort
+    ? acc.movements.slice().sort((a, b) => a - b)
+    : acc.movements;
   movs.forEach(function (move, i) {
     const type = move > 0 ? "deposit" : "withdrawal";
+
+    const date = new Date(acc.movementsDates[i]);
+
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0); //start with zero so 1 must be added
+    const year = date.getFullYear();
+
+    const displayDate = `${day}/${month}/${year}`;
 
     const html = `
     <div class="movements__row">
     <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-    <div class="movements__value">${move}</div>
+    <div class="movements__date">${displayDate}</div>
+    <div class="movements__value">${move.toFixed(2)}</div>
   </div>
 
     `;
@@ -78,11 +99,6 @@ function displayMovement(movements, sort = false) {
     containerMovements.insertAdjacentHTML("afterbegin", html);
   });
 }
-displayMovement(account1.movements);
-
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
 
 const currencies = new Map([
   ["USD", "United States dollar"],
@@ -107,7 +123,7 @@ createUsernames(accounts);
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 console.log("////");
@@ -116,12 +132,12 @@ function calcDisplaySummary(acc) {
   const incomes = acc.movements
     .filter((movement) => movement > 0)
     .reduce((accumulator, movement) => accumulator + movement, 0);
-  labelSumIn.textContent = `${incomes}`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}`;
 
   const out = acc.movements
     .filter((movement) => movement < 0)
     .reduce((accumulator, movement) => accumulator + movement, 0);
-  labelSumOut.textContent = `${Math.abs(out)}`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}`;
 
   const interest = acc.movements
     .filter((mov) => mov > 0)
@@ -135,9 +151,10 @@ function calcDisplaySummary(acc) {
 }
 
 let currentAccount;
+
 function updateUI(currentAccount) {
   //Display movements
-  displayMovement(currentAccount.movements);
+  displayMovement(currentAccount);
 
   ///Display balance
   calcDisplayBalance(currentAccount);
@@ -167,6 +184,13 @@ btnLogin.addEventListener("click", function (e) {
     }`;
     containerApp.style.opacity = 1;
 
+    const now = new Date();
+    const day = `${now.getDate()}`.padStart(2, 0);
+    const month = `${now.getMonth() + 1}`.padStart(2, 0); //start with zero so 1 must be added
+    const year = now.getFullYear();
+    const hours = `${now.getHours()}`.padStart(2, 0);
+    const minutes = `${now.getMinutes()}`.padStart(2, 0);
+    labelDate.textContent = `${day}/${month}/${year}, ${hours}:${minutes}`;
     updateUI(currentAccount);
     //Display movements
 
@@ -197,6 +221,9 @@ btnTransfer.addEventListener("click", function (e) {
     currentAccount.movements.push(-amount);
     receiverAcc.movements.push(amount);
 
+    //add transfer date
+    currentAccount.movementsDates.push(new Date().toISOString());
+    receiverAcc.movementsDates.push(new Date().toISOString());
     //update UI
     updateUI(currentAccount);
     //Display movements
@@ -210,7 +237,7 @@ btnTransfer.addEventListener("click", function (e) {
 btnLoan.addEventListener("click", function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (
     amount > 0 &&
@@ -219,6 +246,8 @@ btnLoan.addEventListener("click", function (e) {
     //true
     currentAccount.movements.push(amount);
 
+    //add transfer date
+    currentAccount.movementsDates.push(new Date().toISOString());
     //update UI
     updateUI(currentAccount);
   }
@@ -250,6 +279,6 @@ btnClose.addEventListener("click", function (e) {
 let sortedState = false;
 btnSort.addEventListener("click", function (e) {
   e.preventDefault();
-  displayMovement(currentAccount.movements, !sortedState);
+  displayMovement(currentAccount, !sortedState);
   sortedState = !sortedState;
 });
