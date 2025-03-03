08528c46e041f0d256e1786b465f33cd2f525444
Phu <Phuripattaraphan3@gmail.com>
2025-03-03 16:21:12 +0700

    fix sorted button

--- script.js
+++ script.js
@@ -61,9 +61,10 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
-function displayMovement(movements) {
+function displayMovement(movements, sort = false) {
   containerMovements.innerHTML = "";
-  movements.forEach(function (move, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  movs.forEach(function (move, i) {
     const type = move > 0 ? "deposit" : "withdrawal";
 
     const html = `
